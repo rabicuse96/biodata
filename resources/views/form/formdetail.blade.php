9cc9d807c35741701b51c47929194cac9487e51b
Ahmad Muzakir Mahmud <rabicuse96@gmail.com>
2018-10-19 21:03:20 +0700

    Perubahan tampilan

--- resources/views/form/formdetail.blade.php
+++ resources/views/form/formdetail.blade.php
@@ -8,18 +8,59 @@
   </head>
   <body>
 
-<div class="pure-menu custom-restricted-width">
-    <span class="pure-menu-heading"></span>
+<form class="pure-form pure-form-aligned" action="/form" method="post">
+  <div class="pure-control-group">
+    <label for="name">Nama</label>
+    <input type="text" name="nama" placeholder="Nama" value="{{$array[0]}}">
+    <?php if ($errors->has('nama')): ?>
+    <p>{{$errors->first('nama')}}</p>
+    <?php endif; ?>
+  </div>
 
-    <ul class="pure-menu-list">
-        <li class="pure-menu-item">{{$array[0]}}</li>
-        <li class="pure-menu-item">{{$array[1]}}</li>
-        <li class="pure-menu-item">{{$array[2]}}</li>
-        <li class="pure-menu-item">{{$array[3]}}</li>
-        <li class="pure-menu-item">{{$array[4]}}</li>
-        <li class="pure-menu-item">{{$array[5]}}</li>
-    </ul>
-</div>
+  <div class="pure-control-group">
+    <label for="name">Email</label>
+    <input type="text" name="email" placeholder="Email" value="{{$array[1]}}">
+    <?php if ($errors->has('email')): ?>
+    <p>{{$errors->first('email')}}</p>
+    <?php endif; ?>
+  </div>
+
+  <div class="pure-control-group">
+    <label for="name">Birth</label>
+    <input type="date" name="birth" value="{{$array[2]}}">
+    <?php if ($errors->has('birth')): ?>
+    <p>{{$errors->first('birth')}}</p>
+    <?php endif; ?>
+  </div>
+
+  <div class="pure-control-group">
+    <label for="name">Telepon</label>
+    <input type="text" name="telepon"placeholder="Telepon" value="{{$array[3]}}">
+    <?php if ($errors->has('telepon')): ?>
+    <p>{{$errors->first('telepon')}}</p>
+    <?php endif; ?>
+
+  </div>
+
+  <div class="pure-control-group">
+      <label for="gender" class="pure-checkbox">Gender</label>
+      <input type="text" name="gender"placeholder="Telepon" value="{{$array[4]}}">
+      <?php if ($errors->has('gender')): ?>
+      <p>{{$errors->first('gender')}}</p>
+      <?php endif; ?>
+  </div>
+
+  <div class="pure-control-group">
+    <label for="alamat">Alamat</label>
+    <textarea name="alamat" class="pure-input-1-2" placeholder="Alamat" rows="8" cols="80">{{$array[5]}}</textarea>
+    <?php if ($errors->has('alamat')): ?>
+    <p>{{$errors->first('alamat')}}</p>
+    <?php endif; ?>
+    <!-- <button type="submit" class="pure-button pure-button-primary" name="submit" value="submit">Submit</button>
+    <button type="reset" class="pure-button pure-button-primary" value="Reset">Reset</button> -->
+  </div>
+  {{csrf_field()}}
+</form>
 
   </body>
 </html>
